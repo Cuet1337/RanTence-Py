45203df834ac6bb3de6f0e0c3f5c90ae9e041122
Cryptos <51972930+Cryptos1337@users.noreply.github.com>
2020-08-14 15:31:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rantence-Cryptos", # Replace with your own username
-    version="0.0.1",
+    name="rantence",
+    version="6.0.0",
     author="Cryptos & Cuet",
     author_email="jenkzzjenkzz@protonmail.com",
     description="A Python package that allows you to generated 1/3 different statements.",
@@ -14,9 +14,10 @@ setuptools.setup(
     url="https://github.com/Cryptos1337/RanTence-Py",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 4",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
+    install_requires=['random'],
 )
